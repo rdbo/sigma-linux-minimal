ef57cab0c61d05f6e1dca0c036d64d959795d4ea
rdbo <rdbodev@gmail.com>
2023-10-28 23:41:02 -0300

    updated init

--- src/make_rootfs.sh
+++ src/make_rootfs.sh
@@ -19,11 +19,15 @@ mkdir -p proc dev sys tmp
 cat << EOF > init
 #!/bin/sh
 
+echo "Starting Sigma Linux..."
 dmesg -n 1
 mount -t devtmpfs none /dev
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t tmpfs none /tmp
+mkdir -p /dev/devpts
+mount -t devpts none /dev/pts
+mdev -s
 setsid cttyhack /bin/sh
 EOF
 chmod +x init
