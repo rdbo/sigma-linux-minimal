5b0275b75ebcf146522ae7031a5fb889212d8896
rdbo <rdbodev@gmail.com>
2023-10-29 17:20:34 -0300

    fixed bash path

--- src/build_bash.sh
+++ src/build_bash.sh
@@ -8,7 +8,7 @@ echo "[*] Modifying bash config..."
 rm -rf "$BASH_BUILD_DIR"
 mkdir -p "$BASH_BUILD_DIR"
 cd "$BASH_BUILD_DIR"
-"$BASH_DIR/configure" --prefix="/usr"
+"$BASH_DIR/configure" --prefix=
 
 echo "[*] Building bash..."
 time make -j "$MAX_THREADS"
