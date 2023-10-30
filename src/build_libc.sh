7b8eaea1cad9b006871eff5a145fdc46a53f674f
rdbo <rdbodev@gmail.com>
2023-10-29 22:41:20 -0300

    compile glibc with linux headers

--- src/build_libc.sh
+++ src/build_libc.sh
@@ -4,11 +4,18 @@
 
 cd "$LIBC_DIR"
 
-echo "[*] Modifying libc config..."
+echo "[*] Cleaning up before libc build..."
 rm -rf "$LIBC_BUILD_DIR"
 mkdir -p "$LIBC_BUILD_DIR"
 cd "$LIBC_BUILD_DIR"
-"$LIBC_DIR/configure" --prefix="/usr"
+
+echo "[*] Installing kernel headers..."
+kernel_headers_dir="$LIBC_BUILD_DIR/kernel-headers"
+mkdir -p "$kernel_headers_dir"
+make -C "$KERNEL_DIR" headers_install INSTALL_HDR_PATH="$kernel_headers_dir"
+
+echo "[*] Configuring libc..."
+"$LIBC_DIR/configure" --prefix="/usr" --with-headers="$kernel_headers_dir/include"
 
 echo "[*] Building libc..."
 time make -j "$MAX_THREADS"
